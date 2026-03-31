5cbe27a34d17b1653286e14419360e35cba6c09e
Blake Fishman <250768620+blakefishman@users.noreply.github.com>
2026-03-30 19:55:10 -0700

    added SQL queries

---
+++ SQL Queries/Cleaning Data for Analysis.sql
@@ -0,0 +1,129 @@
+-- Cleaning and organzing data in Google BigQuery for analysis in Excel and PowerBI, using the Google BigQuery connector.
+    -- One query for each table individually, in order to maintain the original data structure and simplify any future data refreshes.
+
+
+-- 1) Clean orders table
+
+WITH cleaned_orders_table AS (
+  SELECT
+    customer_id,
+    `id`,
+
+    -- Reformat and standardize the purchase dates to an acceptable format for BigQuery dates
+    COALESCE(
+      SAFE.PARSE_DATE('%m/%d/%Y', purchase_ts), -- Convert date string
+      SAFE.PARSE_DATE('%m-%d-%Y', REGEXP_EXTRACT(purchase_ts, r'^\d{2}-\d{2}-\d{4}')) -- Convert timestamp string
+    ) AS cleaned_purchase_date,
+
+    product_id,
+
+    -- Clean and standardize the inconsistent product names
+    CASE
+      WHEN product_name = '27in 4K gaming monitor' THEN '27 Inch 4K Gaming Monitor'
+      WHEN product_name = '27in"" 4k gaming monitor' THEN '27 Inch 4K Gaming Monitor'
+      WHEN product_name = 'bose soundsport headphones' THEN 'Bose Soundsport Headphones'
+      ELSE product_name
+    END AS cleaned_product_name,
+
+    usd_price,
+    local_price,
+
+    -- Classify currency nulls as Unknown
+    COALESCE(currency, 'Unknown') AS cleaned_currency,
+
+    purchase_platform
+FROM projectsandboxtwolocal.TechTreeCore.orders
+QUALIFY ROW_NUMBER() OVER(
+    PARTITION BY `id`
+    ORDER BY cleaned_purchase_date) = 1 -- Deduplicate by id
+)
+
+SELECT *
+FROM cleaned_orders_table
+;
+
+
+-- 2) Clean customers table
+
+WITH cleaned_customers_table AS (
+  SELECT
+    `id`,
+
+    -- Classify marketing channel nulls and empties as unknown
+    CASE 
+      WHEN marketing_channel IS NULL OR marketing_channel = ' ' THEN 'unknown' 
+      ELSE marketing_channel 
+    END AS cleaned_marketing_channel,
+
+    -- Classify account creation method nulls and empties as unknown
+    CASE 
+      WHEN account_creation_method IS NULL OR account_creation_method = ' ' THEN 'unknown' 
+      ELSE account_creation_method 
+    END AS cleaned_account_creation_method,
+
+    -- Classify country code nulls as UNK
+    COALESCE(country_code, 'UNK') AS cleaned_country_code,
+
+    -- Classify country name nulls as Unknown
+    COALESCE(country_name, 'Unknown') AS cleaned_country_name,
+
+    loyalty_program,
+    account_created_on
+FROM projectsandboxtwolocal.TechTreeCore.customers
+QUALIFY ROW_NUMBER() OVER(
+    PARTITION BY `id`
+    ORDER BY account_created_on) = 1 -- Deduplicate by id
+)
+
+SELECT *
+FROM cleaned_customers_table
+;
+
+
+-- 3) Clean order status table
+
+WITH cleaned_order_status_table AS (
+  SELECT
+    order_id,
+
+    -- Reformat and standardize the purchase dates to an acceptable format for BigQuery dates
+    COALESCE(
+      SAFE.PARSE_DATE('%m/%d/%Y', purchase_ts), -- Convert date string
+      SAFE.PARSE_DATE('%m-%d-%Y', REGEXP_EXTRACT(purchase_ts, r'^\d{2}-\d{2}-\d{4}')) -- Convert timestamp string
+    ) AS cleaned_purchase_date,
+
+    ship_ts AS ship_date,
+    delivery_ts AS delivery_date,
+    refund_ts AS refund_date
+FROM projectsandboxtwolocal.TechTreeCore.order_status
+QUALIFY ROW_NUMBER() OVER(
+    PARTITION BY order_id
+    ORDER BY cleaned_purchase_date) = 1 -- Deduplicate by order_id
+)
+
+SELECT
+  order_id,
+  cleaned_purchase_date,
+  ship_date,
+  delivery_date,
+
+    -- Adding metrics for shipping and delivery efficency. Negative durations were discovered, rendered as null, and documented in the issues log.
+  CASE
+    WHEN DATE_DIFF(ship_date, cleaned_purchase_date, DAY) < 0 THEN NULL
+    ELSE DATE_DIFF(ship_date, cleaned_purchase_date, DAY)
+  END AS days_to_ship,
+
+  CASE
+    WHEN DATE_DIFF(delivery_date, cleaned_purchase_date, DAY) < 0 THEN NULL
+    ELSE DATE_DIFF(delivery_date, cleaned_purchase_date, DAY)
+  END AS days_to_delivery,
+
+  refund_date,
+
+    -- Adding refund true/false binary metric
+  CASE
+    WHEN refund_date IS NOT NULL THEN 1
+    ELSE 0
+  END AS refunded
+FROM cleaned_order_status_table
+;
---
+++ SQL Queries/Initial Data Checks and Examining.sql
@@ -0,0 +1,239 @@
+-- Examining dataset in Google BigQuery for the first time and performing various checks for duplicates, errors, inconsistencies, ranges, etc.
+
+
+-- 1) Duplicate orders check (performed for all tables separately)
+
+SELECT
+    orders.id,
+    COUNT(*) AS duplicate_id_count
+FROM core.orders
+GROUP BY 1
+HAVING duplicate_id_count > 1
+;
+
+
+-- 2) Null check (performed for all tables separately)
+
+SELECT
+    SUM(CASE WHEN orders.customer_id IS NULL THEN 1 ELSE 0 END) AS null_count_customer_id,
+    SUM(CASE WHEN orders.id IS NULL THEN 1 ELSE 0 END) AS null_count_id,
+    SUM(CASE WHEN orders.purchase_ts IS NULL THEN 1 ELSE 0 END) AS null_count_purchase_ts,
+    SUM(CASE WHEN orders.product_id IS NULL THEN 1 ELSE 0 END) AS null_count_product_id,
+    SUM(CASE WHEN orders.product_name IS NULL THEN 1 ELSE 0 END) AS null_count_product_name,
+    SUM(CASE WHEN orders.usd_price IS NULL THEN 1 ELSE 0 END) AS null_count_usd_price,
+    SUM(CASE WHEN orders.local_price IS NULL THEN 1 ELSE 0 END) AS null_count_local_price,
+    SUM(CASE WHEN orders.currency IS NULL THEN 1 ELSE 0 END) AS null_count_currency,
+    SUM(CASE WHEN orders.purchase_platform IS NULL THEN 1 ELSE 0 END) AS null_count_purchase_platform
+FROM core.orders
+;
+
+
+-- 3) Empty check for strings (performed for all tables separately)
+
+SELECT
+    SUM(CASE WHEN orders.customer_id = '' THEN 1 ELSE 0 END) AS empty_count_customer_id,
+    SUM(CASE WHEN orders.id = '' THEN 1 ELSE 0 END) AS empty_count_id,
+    SUM(CASE WHEN orders.purchase_ts = '' THEN 1 ELSE 0 END) AS empty_count_purchase_ts,
+    SUM(CASE WHEN orders.product_id = '' THEN 1 ELSE 0 END) AS empty_count_product_id,
+    SUM(CASE WHEN orders.product_name = '' THEN 1 ELSE 0 END) AS empty_count_product_name,
+    SUM(CASE WHEN orders.currency = '' THEN 1 ELSE 0 END) AS empty_count_currency,
+    SUM(CASE WHEN orders.purchase_platform = '' THEN 1 ELSE 0 END) AS empty_count_purchase_platform,
+FROM core.orders
+;
+
+
+-- 4) Check distinct product names, purchase platforms, marketing channels, account creation methods, and loyalty categories for inconsistencies and typos
+
+SELECT
+    DISTINCT product_name
+FROM core.orders
+ORDER BY 1
+;
+
+SELECT
+    DISTINCT purchase_platform
+FROM core.orders
+ORDER BY 1
+;
+
+SELECT
+    DISTINCT marketing_channel,
+    COUNT(marketing_channel) AS marketing_channel_count
+FROM core.customers
+GROUP BY 1
+ORDER BY 1
+;
+
+SELECT
+    DISTINCT account_creation_method,
+    COUNT(account_creation_method) AS account_creation_method_count
+FROM core.customers
+GROUP BY 1
+ORDER BY 1
+;
+
+SELECT
+    DISTINCT loyalty_program
+FROM core.customers
+ORDER BY 1
+;
+
+
+-- 5) Check countries and regions for inconsistencies and typos
+
+SELECT
+    DISTINCT c.country_code,
+    r.region
+FROM core.customers AS c
+LEFT JOIN core.region_lookup AS r
+ON c.country_code = r.country_code
+ORDER BY 1
+;
+
+SELECT
+    DISTINCT c.country_code,
+    COUNT(c.country_code) AS count_null
+FROM core.customers AS c
+LEFT JOIN core.region_lookup AS r
+ON c.country_code = r.country_code 
+WHERE r.region IS NULL
+GROUP BY 1
+;
+
+
+---- 6) Exploration of invalid date formatting for purchase_ts
+
+-- Count string records that would fail CAST conversion to date due to invalid format
+SELECT
+    COUNT(*) AS invalid_count
+FROM core.orders
+WHERE SAFE_CAST(purchase_ts AS DATE) IS NULL -- only return rows that fail to convert
+    AND purchase_ts IS NOT NULL -- only return rows where there is actually data present
+;
+
+-- Identify specific formats from date string
+SELECT
+    CASE
+        WHEN SAFE.PARSE_DATE('%Y-%m-%d', purchase_ts) IS NOT NULL THEN 'YYYY-MM-DD'
+        WHEN SAFE.PARSE_DATE('%m/%d/%Y', purchase_ts) IS NOT NULL THEN 'YYYY/MM/DD'
+        WHEN purchase_ts LIKE '%' THEN 'Timestamp'
+        WHEN purchase_ts IS NULL THEN 'Null'
+        WHEN purchase_ts = '' THEN 'Empty'
+        ELSE 'Other/Invalid'
+    END AS date_format_type,
+    COUNT(*) AS count -- verify total result count is equal to total records
+FROM core.orders
+GROUP BY date_format_type
+;
+
+-- View sample of failed CAST date strings
+SELECT
+    purchase_ts
+FROM core.orders
+WHERE SAFE_CAST(purchase_ts AS DATE) IS NULL
+ORDER BY RAND()
+LIMIT 50
+;
+
+-- View sample of failed CAST timestamp strings
+SELECT
+    purchase_ts
+FROM core.orders
+WHERE SAFE_CAST(purchase_ts AS DATE) IS NULL AND purchase_ts LIKE '%:%'
+ORDER BY RAND()
+LIMIT 50
+;
+
+
+
+-- 7) Examine date ranges for purchases, shipping, delivery, refunds, and accounts created on for outliers
+
+SELECT
+    MIN(purchase_ts) AS earliest_purchase_date,
+    MAX(purchase_ts) AS latest_purchase_date,
+    MIN(ship_ts) AS earliest_ship_date,
+    MAX(ship_ts) AS latest_ship_date,
+    MIN(delivery_ts) AS earliest_delivery_date,
+    MAX(delivery_ts) AS latest_delivery_date,
+    MIN(refund_ts) AS earliest_refund_date,
+    MAX(refund_ts) AS latest_refund_date
+FROM core.order_status
+;
+
+SELECT
+    MIN(account_created_on) AS earliest_acc_created_on,
+    MAX(account_created_on) AS latest_acc_created_on
+FROM core.customers
+;
+
+
+-- 8) Examine price ranges for outliers (also performed for local_price)
+
+SELECT
+    AVG(usd_price) AS avg_price,
+    MAX(usd_price) AS max_price,
+    MIN(usd_price) AS min_price,
+    STDDEV(usd_price) AS stddev_price,
+    VARIANCE(usd_price) AS variance_price
+FROM core.orders
+;
+
+WITH price_validation AS (
+SELECT 
+    CASE 
+        WHEN usd_price IS NULL THEN 'missing_value'
+        WHEN usd_price < 0 THEN 'negative_value'
+        WHEN usd_price = 0 THEN 'zero_value'
+        WHEN usd_price > 10000 THEN 'suspiciously_high'
+        ELSE 'valid'
+    END AS validation_flag
+FROM core.orders
+)
+
+SELECT
+    validation_flag,
+    COUNT(validation_flag) AS validation_flag_count
+FROM price_validation
+GROUP BY 1
+ORDER BY 2 DESC
+;
+
+
+-- 9) Investigate $0 and null transactions further
+
+SELECT
+    id,
+    usd_price,
+    local_price,
+    currency
+FROM core.orders
+WHERE usd_price = 0
+    OR local_price = 0
+;
+
+SELECT
+    id,
+    usd_price,
+    local_price,
+    currency
+FROM core.orders
+WHERE usd_price IS NULL
+    OR local_price IS NULL
+;
+
+
+-- 10) Check for length issues in currency
+
+SELECT currency,
+  LENGTH(currency) AS length
+FROM core.orders
+WHERE LENGTH(currency) != 3
+;
+
+
+-- 11) Check for any orders with invalid customer IDs
+
+SELECT *
+FROM core.orders
+WHERE customer_id NOT IN (SELECT id FROM core.customers)
+;
